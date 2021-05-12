3aeccca9416261bf46fc7992d3903192972db317
tenjodiego961 <79155327+tenjodiego961@users.noreply.github.com>
2021-05-12 14:52:57 -0500

    before minutes all broken :]

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components';
 
 import ImgSlider from './ImgSlider';
 import Viewers from './Viewers';
 import Movies from './Movies';
+import db from '../firebase';
 
 import backgroundHome from '../atends/images/home-background.png';
 
 function Home() {
+
+    useEffect(() => {
+        db.collection("movies").onSnapshot(snapshot => {
+            let tempMovies = snapshot.docs.map(doc => {
+                return { id: doc.id, ...doc.data() }
+            })
+        })
+    }, [])
+
     return (
         <Container>
             <ImgSlider />
